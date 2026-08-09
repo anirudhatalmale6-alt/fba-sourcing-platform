a2110d0973d53f08dbfd9de7dfbdeb795c72f8ad
PetHub Online <noreply@users.noreply.github.com>
2026-08-09 23:54:41 +0000

    Rebrand to Margin Reveal; nginx vhost for marginreveal.co.uk

--- config.py
+++ config.py
@@ -21,9 +21,9 @@ def _flag(key, default=False):
 
 
 # --- Brand -----------------------------------------------------------------
-BRAND_NAME = _env("BRAND_NAME", "Harbourline Sourcing")
+BRAND_NAME = _env("BRAND_NAME", "Margin Reveal")
 BRAND_TAGLINE = _env("BRAND_TAGLINE", "China sourcing and Amazon FBA delivery for UK sellers")
-BRAND_DOMAIN = _env("BRAND_DOMAIN", "")          # e.g. harbourlinesourcing.co.uk
+BRAND_DOMAIN = _env("BRAND_DOMAIN", "marginreveal.co.uk")
 PUBLIC_BASE_URL = _env("PUBLIC_BASE_URL", "http://167.99.198.145:8110").rstrip("/")
 CONTACT_EMAIL = _env("CONTACT_EMAIL", "")         # shown on site only if set
 CONTACT_PHONE = _env("CONTACT_PHONE", "")
